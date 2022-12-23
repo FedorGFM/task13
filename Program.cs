53fb5bf7d8e85ba7eda9a17cc37095ce038f58a2
Fedor <4ips228@gmail.com>
2022-12-24 02:00:09 +0700

    added new 1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,16 @@
-﻿Console.Write
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine()); 
+
+int digit1 = number / 10; 
+int digit2 = number % 10;
+int digit3 = digit1 % 10;
+
+int result = digit1 * 10 + digit2;
+
+if (number >= 0 && number <= 99)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else if (digit1 < 999)
+
+Console.WriteLine($"Вторая цифра из {number} -> {digit3}");
